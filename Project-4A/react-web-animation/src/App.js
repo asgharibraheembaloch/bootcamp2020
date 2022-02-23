ce3a317bab311d2e0121bb50e8fe049f46b39e70
Asghar Ibraheem Baloch <asgharibraheembaloch@gmail.com>
2022-02-23 18:00:24 +0500

    Update App.js

--- Project-4A/react-web-animation/src/App.js
+++ Project-4A/react-web-animation/src/App.js
@@ -1,25 +1,34 @@
-import useWebAnimations from "@wellyshen/use-web-animations";
+import useWebAnimations, { bounce, rotateOut,backInLeft } from "@wellyshen/use-web-animations";
 
-const App = () => {
-  const { ref, playState } = useWebAnimations({
-    keyframes: [
-      {transform: "translate(0px)"},
-      {transform: "translate(500px)"},
-    ],
-    timing:{
-      duration: 3000,
-      iterations: Infinity,
-      direction: "alternate"
-    }
+import React from 'react'
+
+function App() {
+  const { ref, playState, getAnimation } = useWebAnimations({
+    /* keyframes: {
+      transform: "translateX(500px)", // Move by 500px
+      background: ["red", "blue", "green"], // Go through three colors
+    },
+    animationOptions: {
+      delay: 500, // Start with a 500ms delay
+      duration: 1000, // Run for 1000ms
+      iterations: 2, // Repeat once
+      direction: "alternate", // Run the animation forwards and then backwards
+      easing: "ease-in-out", // Use a fancy timing function
+    }, */
+    ...backInLeft
   });
-
   return (
-    <div>      
-      <div ref={ref}>
-        Hello World
+    <div>
+      <div ref={ref} style={{ background: "red" ,width: "100px", height: "100px" }}>
+        hello word {playState}
       </div>
+      <br/>
+      <button onClick={()=>{getAnimation().play()}}>Play</button>
+      <button onClick={()=>{getAnimation().pause()}}>Pause</button>
     </div>
   );
-};
+}
 
-export default App;
+export default App
+
+
