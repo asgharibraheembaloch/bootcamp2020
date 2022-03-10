0d5c18f38b0bb6fe933e21cba79f233915e7d600
Asghar Ibraheem Baloch <asgharibraheembaloch@gmail.com>
2022-03-10 10:56:24 +0500

    created tutorial from bootcamp class

---
+++ timer-app/src/components/App/App.spec.tsx
@@ -0,0 +1,18 @@
+import { shallow } from 'enzyme';
+import App from './App';
+import Timer from '../Timer/Timer';
+
+describe('App', () => {
+  let container:any;
+
+  beforeEach(() => (container = shallow(<App />)));
+
+  it('should render a <div />', () => {
+    const container = shallow(<App />);
+    expect(container.find('div').length).toEqual(1);
+  });
+
+  it('should render the Timer component', () => {
+    expect(container.containsMatchingElement(<Timer />)).toEqual(true);
+  });
+});
